4ec9a772c41fc561360922bff4b5e0bb405f0a31
aggillator <92499958+aggillator@users.noreply.github.com>
2026-07-17 07:46:47 -0400

    Delete functions/api/accounts.js

--- functions/api/accounts.js
+++
@@ -1,6 +0,0 @@
-import { json, sb } from "../_utils.js";
-
-export async function onRequestGet({ env }) {
-  const rows = await sb(env, "accounts?select=id,name,mask,type&order=name.asc");
-  return json(rows);
-}
